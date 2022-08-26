a3ea0cb0e8cf9c8aa5e9b67204bb4c55d553871b
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2022-08-26 11:28:26 -0700

    Added Script files

---
+++ Number-guessing-game-GUI/Assets/Scripts/NumberGuessingGame.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGuessingGame : MonoBehaviour
+{
+    private int guess = 5;
+    private int minValue = 1;
+    private int maxValue = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minValue = guess;
+            guess = (maxValue + minValue) /2;
+            Debug.Log("Is your number higher or lower than: " + guess + "?");
+        }
+        //Once you go up, down will subtract a number once and repeat
+        //Same goes for down
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxValue = guess;
+            guess = (maxValue + minValue) /2;
+            Debug.Log("Is your number higher or lower than: " + guess + "?");
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Weow! The number was correct!");
+            Debug.Log("");
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        guess = 5;
+        minValue = 1;
+        maxValue = 10;
+        Debug.Log("Welcome to our Number Guessing game!");
+        Debug.Log("The range of numbers is between " + minValue + " and "  + maxValue);
+        Debug.Log("Use the up and down arrow keys on your keyboard to guess if you think the number is higher or lower than: " + guess + "?");
+        Debug.Log("Good luck and remember to have fun! (To an extent of course.)");
+        maxValue = maxValue + 1;
+    }
+}
